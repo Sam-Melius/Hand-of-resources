2c24666845705edddf4917f7d0267faf9a07c1bc
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-03-18 11:34:50 -0700

    wrote delete cat. Passing

--- __tests__/cat.test.js
+++ __tests__/cat.test.js
@@ -37,4 +37,11 @@ describe('Hand-of-resources routes', () => {
     expect(res.body).toEqual(expected);
   });
 
+  it('deletes a cat by id', async () => {
+    const expected = await Cat.findById(1);
+    const res = await request(app).delete(`/api/v1/cats/${expected.id}`);
+
+    expect(res.body).toEqual(expected);
+  });
+
 });
